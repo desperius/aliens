0c382a033342eed754869009cb95d878c393e7b7
desperius <desperius@gmail.com>
2019-01-02 22:29:35 +0200

    Initial commit

---
+++ aliens.py
@@ -0,0 +1,24 @@
+""" Maim module """
+
+import sys
+import pygame
+
+def run_game():
+    """ Starts main loop """
+    pygame.init()
+
+    screen = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption('Alien Invasion')
+
+    bg_color = (230, 230, 230)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    # Swap buffers
+    pygame.display.flip()
+
+
+run_game()
