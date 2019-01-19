5449b9a1fe7bfe53e0ddad0e4f64deeaaef5abd1
Alexander Orel <desperius@gmail.com>
2019-01-19 15:07:09 +0200

    Ship moves implemented

--- aliens.py
+++ aliens.py
@@ -1,10 +1,10 @@
 """ Maim module """
 
-import sys
 import pygame
 
 from settings import Settings
 from ship import Ship
+import utilities as gf
 
 def run_game():
     """ Starts main loop """
@@ -13,18 +13,13 @@ def run_game():
     ai_settings = Settings()
 
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
     pygame.display.set_caption('Alien Pie :)')
 
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-
-        # Swap buffers
-        pygame.display.flip()
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
 
 
 run_game()
--- ship.py
+++ ship.py
@@ -4,15 +4,28 @@ import pygame
 
 class Ship():
     """ Class for ship """
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         """ Initialize ship and its settings """
         # Load image and get rect
         self.screen = screen
         self.image = pygame.image.load("images/ship.bmp")
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
+        self.ai_settings = ai_settings
         self.rect.centerx = self.screen_rect.centerx
+        self.center = float(self.rect.centerx)
         self.rect.bottom = self.screen_rect.bottom
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """ Update ship position """
+        if self.moving_right and (self.rect.right < self.screen_rect.right):
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left and (self.rect.left > self.screen_rect.left):
+            self.center -= self.ai_settings.ship_speed_factor
+
+        self.rect.centerx = self.center
 
     def blitme(self):
         """ Render ship """
